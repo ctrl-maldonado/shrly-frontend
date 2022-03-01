b7417eac8e7dcb420e55c733db96da746551e61f
Alejandro Maldonado <91658941+reychu@users.noreply.github.com>
2022-03-01 18:53:05 +0100

    Initial frontend - setting up connection to backend

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'sharely-frontend';
+  public users: User[];
+  
+  constructor(private userService: UserService) { }
+  
+  ngOnInit() {
+    //debugger;
+    this.getUsers(); 
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    userId: number;
+    userName: string;
+    creationDate: number;
+    password: string;
+    university: number;
+    email: string;
+}
